15bd715fc6f70acb078ae3ae3170a51f35fcd5da
mnederkorn <maxime.nederkorn@stud.uni-due.de>
2018-02-21 04:33:22 +0100

    Initial commit of core-functionality and accompanying GUI
    Basic Graph representation, as well as generating, parsing, serializing, visualizing, (pretty) printing them.
    Finding a Graph's core with limboole via shell, as well with z3 via z3py.

---
+++ cores.py
@@ -0,0 +1,381 @@
+import random
+import re
+import subprocess
+import time
+import copy
+import os
+
+from graphviz import Digraph
+from tempfile import TemporaryFile
+from z3 import *
+
+class Graph:
+
+	def __init__(self, parse=None, graph=None, gen=None):
+
+		if parse == None and graph == None and gen == None:
+			raise Exception("You have to parse, copy or generate a Graph.")
+		elif ((parse != None and graph != None) or (parse != None and gen != None) or (graph != None and gen != None)):
+			raise Exception("You have to parse OR copy OR generate a Graph.")
+		elif parse != None:
+			if isinstance(parse, str):
+				self.graph = self._parse_file(parse)
+			else:
+				raise Exception("""The parameter parse has to be a absolute filepath in the form of a string. E.g.:
+new_graph = Graph(parse=r\"C:\\yourgraph.txt\") or
+new_graph = Graph(parse=\"C:\\\\yourgraph.txt\")""")
+		elif graph != None:
+			if ((isinstance(graph, dict) and len(graph)>0) and
+				all(isinstance(graph[n], dict) and (re.fullmatch(r"\d+(\-\d+)*", n)) for n in graph) and
+				all(((m in graph) and isinstance(m, set) and len(m)>0) for n in graph for m in graph[n]) and
+				all((isinstance(k, str) and re.fullmatch(r"[A-Z]", k)) for n in graph for m in graph[n] for l in graph[n][m] for k in l) and
+				all(m in graph for n in graph for m in graph[n])):
+				self.graph = copy.deepcopy(graph)
+			else:
+				raise Exception("""The graph parameter should only be used to copy other preexisting Graphs. E.g.:
+new_graph = Graph(graph=YourGraphInstance.graph)""")
+		elif gen != None:
+			if ((isinstance(gen, tuple) and len(gen)==3) and
+				(isinstance(gen[0], int) and gen[0]>0) and
+				(isinstance(gen[1], int) and gen[1]>0 and gen[1]<=26) and
+				((isinstance(gen[2], int) or isinstance(gen[2], float)) and gen[2]>=0 and gen[2]<=gen[0]*gen[1])):
+				self.graph = self._generate(*gen)
+			else:
+				raise Exception("""The gen parameter has to be of the form of a tuple (nodes_n, labels_n, avg_edges_out) with:
+int: nodes_n>0 representing the desired amount of nodes,
+int: 26>=labels_n>0 representing the desired amount of labels and
+float: nodes_n*labels_n>=avg_edges_out>=0 representing the desired approximate average amount of edges leaving each node. G.g.:
+new_graph = Graph(gen=(8,2,1.1))""")
+
+	@staticmethod
+	def _generate(nodes_n, labels_n, avg_edges_out):
+
+		ratio = avg_edges_out/(nodes_n*labels_n)
+
+		graph = {}
+
+		for n in range(nodes_n):
+			graph[str(n+1)] = {}
+			for m in range(nodes_n):
+				for l in range(labels_n):
+					if(random.random()<ratio):
+						if str(m+1) in graph[str(n+1)]:
+							graph[str(n+1)][str(m+1)].add(chr(l+65))
+						else:
+							graph[str(n+1)][str(m+1)] = set(chr(l+65))
+
+		return graph
+
+	@staticmethod
+	def _parse_file(target_path):
+
+		try:
+			data = open(target_path, "r")
+		except Exception as e:
+			print(e)
+			return
+
+		return Graph._parse(data)		
+
+	@staticmethod
+	def _parse(data):
+
+		line = data.readline()
+		if not re.fullmatch(r"(\d+(\-\d+)*( \d+(\-\d+)*)*|\[[1-9][0-9]*\]) ?(\n)?", line):
+			data.close()
+			raise Exception("First line has to be of the form:\n(\d+(\-\d+)*( \d+(\-\d+)*)*|\[[1-9][0-9]*\]) ?(\\n)?")
+		if "[" in line:
+			nodes = [str(n) for n in range(1, int(line.rstrip("\n").lstrip("[").rstrip("]"))+1)]
+		else:
+			nodes = line.split()
+		nodes_or = "|".join(nodes)
+
+		for i,n in enumerate(nodes):
+			if n in nodes[i+1:len(nodes)]:
+				data.close()
+				raise Exception("Each node has to be distinct")
+
+		graph = {n: {} for n in nodes}
+
+		edges = data.read()
+
+		if len(edges)>0:
+
+			data.close()
+			if not re.fullmatch(r"(("+nodes_or+") ("+nodes_or+") [A-Z] ?\n)*(("+nodes_or+") ("+nodes_or+") [A-Z] ?\n?)?", edges):
+				raise Exception("Second to last line have to be in the form of:\n(("+nodes_or+") ("+nodes_or+") [A-Z] ?\\n)*(("+nodes_or+") ("+nodes_or+") [A-Z] ?\\n?)?")
+			edges = edges.rstrip("\n").split("\n")
+
+			for i,n in enumerate(edges):
+				if n in edges[i+1:len(edges)]:
+					data.close()
+					raise Exception("Each edge has to be distinct")
+					
+			edges = [n.split() for n in edges]
+
+			for n in edges:
+				if n[1] in graph[n[0]]:
+					graph[n[0]][n[1]].add(n[2])
+				else:
+					graph[n[0]][n[1]] = set(n[2])
+
+		return graph
+
+	def print(self, length, style):
+
+		if length==1:
+			print("["+str(len(self.graph))+"]")
+
+		if style==1:
+			print(self.graph)
+		elif style == 2:
+			for n in self.graph:
+				src = n
+				for m in self.graph[n]:
+					tgt = " ("+m
+					for l in self.graph[n][m]:
+						tgt += " "+l
+					tgt += ")"
+					src += tgt
+				print(src)
+
+	def visualize(self, target_path=None):
+
+		if target_path == None:
+			target_path = os.path.dirname(os.path.realpath(__file__))+"\\images\\"+time.strftime("%Y%m%d%H%M%S",time.localtime())
+
+		view = Digraph(format="png")
+
+		for n in self.graph:
+			view.node(n)
+			for m in self.graph[n]:
+				label = ""
+				for l in sorted(self.graph[n][m]):
+					label += l
+				view.edge(n,m,label)
+
+		try:
+			view.render(filename=target_path, view=False, cleanup=True)
+			return target_path
+		except Exception as e:
+			print(e)
+			return
+
+	def serialize(self, target_path=None):
+
+		if target_path == None:
+			target_path = os.path.dirname(os.path.realpath(__file__))+r"\\graphs\\"+time.strftime("%Y%m%d%H%M%S",time.localtime())+".txt"
+
+		try:
+			file = open(target_path, "w")
+		except Exception as e:
+			print(e)
+			return
+
+		for n in self.graph:
+			file.write(n+" ")
+
+		file.write("\n")
+
+		for n,m,l in [(n,m,l) for n in self.graph for m in self.graph[n] for l in self.graph[n][m]]:
+			file.write(n+" "+m+" "+l+"\n")
+
+		file.close()
+		return target_path
+
+	def reduce(self, mappings):
+
+		mappings = [n for n in mappings if n[0]!=n[1]]
+
+		mappings = [[n[0]+"".join(["-"+m[0] for m in mappings[:i] if m[1] == n[0]]),n[1]+"".join(["-"+m[0] for m in mappings[:i] if m[1] == n[1]])] for i,n in enumerate(mappings)]
+
+		for n in mappings:
+
+			del self.graph[n[0]]
+			self.graph[n[1]+"-"+n[0]] = self.graph.pop(n[1])
+
+			for m in self.graph:
+
+				if n[0] in self.graph[m]:
+					del self.graph[m][n[0]]
+				if n[1] in self.graph[m]:
+					self.graph[m][n[1]+"-"+n[0]] = self.graph[m].pop(n[1])
+
+	def __i_limboole(self):
+
+		tempfile = TemporaryFile(mode="w+")
+
+		for n in self.graph:
+			tempfile.write("(\n\t(")
+			for i,m in enumerate(self.graph):
+				if not m == n:
+					tempfile.write("!.@"+n+"_@"+m)
+				else:
+					tempfile.write(".@"+n+"_@"+m)
+				if not i+1 == len(self.graph):
+					tempfile.write(" & ")
+			tempfile.write(") |\n\t(\n\t\t(\n")
+			for i,m in enumerate([m for m in self.graph if m != n]):
+				tempfile.write("\t\t\t(")
+				for j,l in enumerate(self.graph):
+					if not l == m:
+						tempfile.write("!.@"+n+"_@"+l)
+					else:
+						tempfile.write(".@"+n+"_@"+l)
+					if not j+1 == len(self.graph):
+						tempfile.write(" & ")
+				if not i+2 == len(self.graph):
+					tempfile.write(") |\n")
+			tempfile.write(")\n\t\t)\n\t\t&\n\t\t(")
+			for i,m in enumerate(self.graph):
+				tempfile.write("!.@"+m+"_@"+n)
+				if not i+1 == len(self.graph):
+					tempfile.write(" & ")
+			tempfile.write(")\n\t)\n)\n&\n")
+
+		edges = [[n,m] for n in self.graph for m in self.graph[n]]
+		for n in edges:
+			tempfile.write("(\n")
+			edges_group = [m for m in edges if (self.graph[m[0]][m[1]] >= self.graph[n[0]][n[1]])]
+			for i,m in enumerate(edges_group):
+				tempfile.write("\t(.@"+n[0]+"_@"+m[0]+" & .@"+n[1]+"_@"+m[1]+")")
+				if not i+1 == len(edges_group):
+					tempfile.write(" |\n")
+			tempfile.write("\n)\n&\n")
+
+		tempfile.write("(\n")
+		for i,n in enumerate(self.graph):
+			tempfile.write("\t(")
+			for j,m in enumerate(self.graph):
+				tempfile.write("!.@"+m+"_@"+n)
+				if not j+1 == len(self.graph):
+					tempfile.write(" & ")
+			if not i+1 == len(self.graph):
+				tempfile.write(") |\n")
+		tempfile.write(")\n)")
+
+		tempfile.seek(0)
+
+		result = subprocess.run("limboole -s", stdin=tempfile, stdout=subprocess.PIPE, shell=True)
+
+		tempfile.close()
+
+		return result.returncode, result.stdout.decode()
+
+	def __o_limboole(self, result):
+
+		mappings = re.findall(r".(@\d+(?:-\d+)*_@\d+(?:-\d+)*) = 1\r\n", result)
+
+		mappings = [[m.lstrip("@") for m in n.split("_")] for n in mappings]
+
+		self.reduce(mappings)
+
+	def solve(self):
+
+		core = False
+
+		while not core:
+
+			if (len(self.graph)==1):
+				core = True
+				continue
+
+			returncode, result = self.__i_limboole()
+
+			if returncode!=0:
+				print("I'm sorry, but CoReS wasn't able to solve your problem.")
+				exit()
+			else:
+				if result.startswith("% SATISFIABLE"):
+					self.__o_limboole(result)
+				elif result.startswith("% UNSATISFIABLE"):
+					core = True
+
+	def __z3(self):
+
+		c = Context()
+
+		edges = list(enumerate([(n,m,l) for n in self.graph for m in self.graph[n] for l in self.graph[n][m]]))
+
+		labels = list(enumerate({n for m in [self.graph[n][m] for n in self.graph for m in self.graph[n]] for n in m}))
+
+		V = Datatype("V",ctx=c)
+		if not len(edges) == 0:
+			E = Datatype("E",ctx=c)
+			L = Datatype("L",ctx=c)
+
+		vertices = {}
+
+		for n in enumerate(self.graph):
+			V.declare("V"+n[1])
+			vertices[n[1]] = n[0]
+		V = V.create()
+
+		if not len(edges) == 0:
+			for n in edges:
+				E.declare("E"+str(n[0]))
+			E = E.create()
+
+			labels_2 = {}
+
+			for n in labels:
+				L.declare("L"+n[1])
+				labels_2[n[1]] = n[0]
+			L = L.create()
+
+
+		if not len(edges) == 0:
+			src = Function("src", E, V)
+			tgt = Function("tgt", E, V)
+			lab = Function("lab", E, L)
+
+		s = Solver(ctx=c)
+
+		for n in edges:
+			s.add(src(E.constructor(n[0])())==V.constructor(vertices[n[1][0]])())
+			s.add(tgt(E.constructor(n[0])())==V.constructor(vertices[n[1][1]])())
+			s.add(lab(E.constructor(n[0])())==L.constructor(labels_2[n[1][2]])())
+
+		vmorph = Function("vmorph", V, V)
+		if not len(edges) == 0:
+			emorph = Function("emorph", E, E)
+
+			e = Const("e", E)
+
+			s.add(ForAll(e, (src(emorph(e))==vmorph(src(e)))))
+			s.add(ForAll(e, (tgt(emorph(e))==vmorph(tgt(e)))))
+			s.add(ForAll(e, (lab(emorph(e))==lab(e))))
+
+		v1, v2 = Consts("v1 v2", V)
+		s.add(Exists(v1, (Not (Exists (v2, (v1==vmorph(v2)))))))
+		s.add(ForAll(v1, (Implies ((Exists (v2, v1==vmorph(v2))),(v1==vmorph(v1))))))
+
+		if not len(edges) == 0:
+			e1, e2 = Consts("e1 e2", E)
+
+			s.add(ForAll(e1, (Implies ((Exists (e2, e1==emorph(e2))),(e1==emorph(e1))))))
+
+		mappings = []
+
+		if s.check() == sat:
+			m = s.model()
+			for n in vertices:
+				if ("V"+n != str(m.eval(vmorph(V.constructor(vertices[n])())))):
+					mappings.append([n, str(m.eval(vmorph(V.constructor(vertices[n])()))).lstrip("V")])
+		else:
+			return False
+
+		self.reduce(mappings)
+
+		return True
+
+	def z3solve(self):
+
+		core = False
+
+		while not core:
+
+			if self.__z3():
+				continue
+			else:
+				core = True
---
+++ gui.py
@@ -0,0 +1,305 @@
+import os
+import copy
+
+from cores import Graph as graph
+from tkinter import *
+from tkinter import filedialog
+from tkinter import messagebox
+from tkinter import ttk
+from PIL import Image, ImageTk
+from tempfile import TemporaryFile
+
+zoom_factor = (2**(1/2))
+
+class Gui:
+
+	def __init__(self):
+
+		self.top = Tk()
+
+		self.menubar = Menu(self.top)
+		self.filemenu = Menu(self.menubar, tearoff=0)
+		self.newmenu = Menu(self.filemenu, tearoff=0)
+		self.newmenu.add_command(label="Empty", command=self.new_empty, accelerator="Ctrl-N")
+		self.newmenu.add_command(label="Generate", command=self.new_generate, accelerator="Ctrl-G")
+		self.filemenu.add_cascade(label="New", menu=self.newmenu)
+		self.filemenu.add_command(label="Open", command=self.open_file, accelerator="Ctrl-O")
+		self.filemenu.add_command(label="Save", command=self.save_file, accelerator="Ctrl-S")
+		self.filemenu.add_command(label="Save as", command=self.save_file_as, accelerator="Ctrl-Shift-S")
+		self.menubar.add_cascade(label="File", menu=self.filemenu)
+		self.top.config(menu=self.menubar)
+
+		self.frame_left = Frame(self.top)
+		self.button_frame = Frame(self.frame_left)
+		self.render_btn = Button(master=self.button_frame, command=self.render, text="Render")
+		self.core_btn = Button(master=self.button_frame, command=self.get_core, text="Get Core")
+		self.text = Text(master=self.frame_left, width=30, height=20, wrap=NONE, undo=True)
+		self.scrollx = Scrollbar(master=self.frame_left, command=self.text.xview, orient=HORIZONTAL)
+		self.scrolly = Scrollbar(master=self.frame_left, command=self.text.yview, orient=VERTICAL)
+		self.text.config(xscrollcommand=self.scrollx.set, yscrollcommand=self.scrolly.set)
+
+		self.canvas = Canvas(master=self.top)
+		self.canvas.config(width=400)
+
+		self.render_btn.pack(side=LEFT, fill=X, expand=True)
+		self.core_btn.pack(side=RIGHT, fill=X, expand=True)
+		self.button_frame.grid(row=1, column=1, sticky="we")
+		self.text.grid(row=2, column=1, sticky="ns")
+		self.scrollx.grid(row=3, column=1, sticky="we")
+		self.scrolly.grid(row=2, column=2, sticky="ns")
+		self.frame_left.rowconfigure(2, weight=1)
+		
+		self.frame_left.pack(side=LEFT, fill=Y, expand=False)
+
+		self.canvas.pack(side=RIGHT, fill=BOTH, expand=True)
+		self.canvas.update()
+
+		self.canvas.bind("<ButtonPress-1>", self.scroll_start)
+		self.canvas.bind("<B1-Motion>", self.scroll_move)
+		self.canvas.bind("<MouseWheel>",self.zoom)
+		self.top.bind_all("<Control-Key-n>", self.new_empty)
+		self.top.bind_all("<Control-Key-g>", self.new_generate)
+		self.top.bind_all("<Control-Key-o>", self.open_file)
+		self.top.bind_all("<Control-Key-s>", self.save_file)
+		self.top.bind_all("<Control-Key-S>", self.save_file_as)
+		self.top.bind_all("<Control-Key-r>", self.render)
+		self.top.bind_all("<Control-Key-c>", self.get_core)
+		self.top.bind_all("<Control-Key-z>", self.get_core)
+
+		self.new_empty()
+
+		self.top.mainloop()
+
+	def title(self):
+
+		if not self.file:
+			self.top.title("tmp - CoReS")
+		else:
+			self.top.title(self.file+" - CoReS")
+
+	def to_text(self):
+
+		self.text.delete(1.0,END)
+
+		if not self.graph == None:
+
+			for n in self.graph.graph:
+				self.text.insert(END, n+" ")
+
+			for (n,m,l) in [(n,m,l) for n in self.graph.graph for m in self.graph.graph[n] for l in self.graph.graph[n][m]]:
+				self.text.insert(END, "\n"+n+" "+m+" "+l)
+
+	def from_text(self):
+
+		tempfile = TemporaryFile(mode="w+")
+		tempfile.write(self.text.get("1.0",'end-1c'))
+		tempfile.seek(0)
+		try:
+			self.graph = graph(graph=graph._parse(tempfile))
+		except Exception as e:
+			messagebox.showerror("Error",e)
+			return False
+		tempfile.close()
+		return True
+
+	def render(self, *_):
+
+		if self.from_text():
+			tmp = self.graph.visualize()
+			self.img = Image.open(tmp+".png")
+			self.re_render()
+		else:
+			return
+
+	def re_render(self):
+
+		self.img_cache=self.img.resize((int(self.img.size[0]*self.scale),int(self.img.size[1]*self.scale)),Image.ANTIALIAS)
+		self.pi = ImageTk.PhotoImage(self.img_cache)
+		self.canvas.delete("all")
+		self.canvas.create_image(0, 0, anchor="nw", image=self.pi)
+		self.canvas.config(scrollregion=self.canvas.bbox("all"))
+
+		if self.scale >= 1:
+			self.canvas.config(width=max(self.top.winfo_screenwidth()/4,min(self.img_cache.width, self.top.winfo_screenwidth()/2**(1/2))), height=min(self.img_cache.height, self.top.winfo_screenheight()/2**(1/2)))
+
+	def get_core(self, *e):
+
+		if self.from_text():
+
+			old = copy.deepcopy(self.img)
+
+			l = Label(master=self.top, text="Searching for Core...", background="orange", font=("TkDefaultFont",24))
+
+			l.place(x=0,y=0)
+
+			self.top.update()
+
+			w,h = int(l.winfo_width()/2),int(l.winfo_height()/2)
+
+			l.place(relx=0.5, rely=0.5, x=-w, y=-h)
+
+			self.top.update()
+
+			if (len(e) ==1 and e[0].keysym == "z"):
+				self.graph.z3solve()
+			else:
+				self.graph.solve()
+
+			l.destroy()
+
+			self.to_text()
+			self.render()
+
+			OldGui(old)
+
+		else:
+			return
+
+	def new_empty(self, *_):
+
+		self.file = None
+		self.graph = None
+		self.title()
+		self.to_text()
+		self.scale=1.0
+		self.canvas.delete("all")
+
+	def generate(self, nodes_n,labels_n,avg_out):
+
+		self.file = None
+		self.graph = graph(gen=(nodes_n,labels_n,avg_out))
+		self.title()
+		self.to_text()
+		self.scale=1.0
+		self.render()
+
+	def new_generate(self,*_):
+
+		top = Toplevel()
+		top.resizable(False, False)
+		top.title("Generate Graph")
+		nodes_lab = Label(top, text="Nodes #")
+		nodes_n = Spinbox(top, from_=1, to=sys.maxsize)
+		nodes_n.delete(0, 1)
+		nodes_n.insert(0, 8)
+		labels_lab = Label(top, text="Labels #", anchor="w")
+		labels_n = Spinbox(top, from_=1, to=sys.maxsize)
+		labels_n.delete(0, 1)
+		labels_n.insert(0, 2)
+		out_lab = Label(top, text="Avg. Edges per Node   ", anchor="w")
+		avg_out = Spinbox(top, from_=0, to=sys.maxsize, increment=0.05)
+		avg_out.delete(0, 4)
+		avg_out.insert(0, 1.1)
+		button = Button(top, text="Generate", command=lambda: [self.generate(int(nodes_n.get()),int(labels_n.get()),float(avg_out.get())), top.destroy()])
+
+		nodes_lab.grid(row=1,column=1,sticky="w")
+		labels_lab.grid(row=2,column=1,sticky="w")
+		out_lab.grid(row=3,column=1,sticky="w")
+		nodes_n.grid(row=1,column=2)
+		labels_n.grid(row=2,column=2)
+		avg_out.grid(row=3,column=2)
+		button.grid(row=4,column=1,columnspan=2,sticky="we")
+
+		button.bind("<Return>", lambda _: [self.generate(int(nodes_n.get()),int(labels_n.get()),float(avg_out.get())), top.destroy()])
+
+		button.focus()
+
+		top.mainloop()
+
+	def open_file(self, *_):
+
+		file = filedialog.askopenfilename(parent=self.top, initialdir=os.path.dirname(os.path.realpath(__file__)), filetypes=[("Plain Text", ".txt"),("All Files", ".*")])
+
+		if file:
+			self.file=file
+			self.graph = graph(parse=file)
+			self.title()
+			self.to_text()
+			self.scale=1.0
+			self.render()
+
+	def save_file(self, *_):
+
+		if self.file:
+			if self.from_text():
+				self.graph.serialize(self.file)
+				self.title()
+				self.render()
+			else:
+				return
+		else:
+			self.save_file_as()
+
+	def save_file_as(self, *_):
+
+		if self.graph == None:
+			messagebox.showwarning("Attention","You can't save an empty graph.")
+			return
+
+		self.file = filedialog.asksaveasfilename(parent=self.top, initialdir=os.path.dirname(os.path.realpath(__file__)), filetypes=[("Plain Text", ".txt"),("All Files",".*")], defaultextension=".txt")
+		if self.file:
+			if self.from_text():
+				self.graph.serialize(self.file)
+				self.title()
+				self.render()
+			else:
+				return
+
+	def scroll_start(self, event):
+	    self.canvas.scan_mark(event.x, event.y)
+
+	def scroll_move(self, event):
+	    self.canvas.scan_dragto(event.x, event.y, gain=1)
+
+	def zoom(self, e):
+
+		if (zoom_factor**-4)<=self.scale*(zoom_factor**(e.delta/120))<=(zoom_factor**2):
+			self.scale*=(zoom_factor**(e.delta/120))
+			self.re_render()
+
+class OldGui:
+
+	def __init__(self, img):
+
+		self.old = Toplevel()
+		self.old.title("Original Graph")
+		self.canvas = Canvas(master=self.old)
+		self.scale = 1.0
+		self.img = img
+
+		self.canvas.bind("<ButtonPress-1>", self.scroll_start)
+		self.canvas.bind("<B1-Motion>", self.scroll_move)
+		self.canvas.bind("<MouseWheel>",self.zoom)
+
+		self.canvas.pack(fill=BOTH, expand=True)
+
+		self.re_render()
+
+		self.old.mainloop()
+
+
+	def scroll_start(self, event):
+		self.canvas.scan_mark(event.x, event.y)
+
+	def scroll_move(self, event):
+		self.canvas.scan_dragto(event.x, event.y, gain=1)
+
+	def zoom(self, e):
+		if (zoom_factor**-4)<=self.scale*(zoom_factor**(e.delta/120))<=(zoom_factor**2):
+			self.scale*=(zoom_factor**(e.delta/120))
+			self.re_render()
+
+	def re_render(self):
+
+		self.img_cache=self.img.resize((int(self.img.size[0]*self.scale),int(self.img.size[1]*self.scale)),Image.ANTIALIAS)
+		self.pi = ImageTk.PhotoImage(self.img_cache)
+		self.canvas.delete("all")
+		self.canvas.create_image(0, 0, anchor="nw", image=self.pi)
+		self.canvas.config(scrollregion=self.canvas.bbox("all"))
+
+		if self.scale >= 1:
+			self.canvas.config(width=max(self.old.winfo_screenwidth()/4,min(self.img_cache.width, self.old.winfo_screenwidth()/2**(1/2))), height=max(self.old.winfo_screenheight()/4, min(self.img_cache.height, self.old.winfo_screenheight()/2**(1/2))))
+
+if __name__ == '__main__':
+
+	Gui()
